ccbaeceadfad3e3d80492d698905bdd5479f2945
inushima <8144028+inushima@users.noreply.github.com>
2022-11-04 00:11:27 +0900

    コードの制約設定を削除

--- Netflix Clone/Controllers/General/TitlePreviewViewController.swift
+++ Netflix Clone/Controllers/General/TitlePreviewViewController.swift
@@ -25,48 +25,11 @@ class TitlePreviewViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        //　ナビゲーションバーの背景色
-//        configureConstraints()
         guard let viewModel = viewModel else { return }
         configure(with: viewModel)
     }
     
 
-    
-    func configureConstraints() {
-        let webViewConstraints = [
-            webView.topAnchor.constraint(equalTo: view.topAnchor, constant: 50),
-            webView.leadingAnchor.constraint(equalTo: view.leadingAnchor),
-            webView.trailingAnchor.constraint(equalTo: view.trailingAnchor),
-            webView.heightAnchor.constraint(equalToConstant: 300)
-        ]
-        
-        let titleLabelConstraints = [
-            titleLabel.topAnchor.constraint(equalTo: webView.bottomAnchor, constant: 20),
-            titleLabel.leadingAnchor.constraint(equalTo: view.leadingAnchor, constant: 20),
-        ]
-        
-        let overviewLabelConstraints = [
-            overviewLabel.topAnchor.constraint(equalTo: titleLabel.bottomAnchor, constant: 15),
-            overviewLabel.leadingAnchor.constraint(equalTo: view.leadingAnchor, constant: 20),
-            overviewLabel.trailingAnchor.constraint(equalTo: view.trailingAnchor)
-        ]
-        
-        let downloadButtonConstraints = [
-            downloadButton.centerXAnchor.constraint(equalTo: view.centerXAnchor),
-            downloadButton.topAnchor.constraint(equalTo: overviewLabel.bottomAnchor, constant: 25),
-            downloadButton.widthAnchor.constraint(equalToConstant: 140),
-            downloadButton.heightAnchor.constraint(equalToConstant: 40)
-        ]
-        
-        NSLayoutConstraint.activate(webViewConstraints)
-        NSLayoutConstraint.activate(titleLabelConstraints)
-        NSLayoutConstraint.activate(overviewLabelConstraints)
-        NSLayoutConstraint.activate(downloadButtonConstraints)
-        
-    }
-    
-    
     public func configure(with model: TitlePreviewViewModel) {
         titleLabel.text = model.title
         titleLabel.sizeToFit()
